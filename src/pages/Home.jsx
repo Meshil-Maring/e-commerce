0fdf11fe3f5d124de13663004f2a80c1914048ac
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-04-25 21:53:32 +0530

    Added folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">Hello, World!</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+
+export default Home;
