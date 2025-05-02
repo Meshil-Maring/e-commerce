d98f07912097e4621b388f1e0f74aabc42a69165
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-02 22:35:44 +0530

    update flash sale

--- src/components/mobile/home/FlashSale.jsx
+++ src/components/mobile/home/FlashSale.jsx
@@ -2,40 +2,41 @@ const FlashSale = () => {
   return (
     <div>
       <div className="flex w-full justify-between items-center p-4 py-0 mt-8">
-        <h3 className="text-2xl font-medium">Flash Sale</h3>
-        <p className="flex gap-2 justify-center items-center text-[var(--text-secondary)]">
+        <h3 className="text-md font-medium">Flash Sale</h3>
+        <p className="flex gap-2 text-sm justify-center items-center text-[var(--text-secondary)]">
           Closing in:
-          <span className="p-2 px-4 rounded-md text-white bg-[#bdbdbd]">
+          <span className="p-2 text-sm px-2 rounded-md text-white bg-[#bdbdbd]">
             02
           </span>
           <span>:</span>
-          <span className="p-2 px-4 rounded-md text-white bg-[#bdbdbd]">
+          <span className="p-2 text-sm px-2 rounded-md text-white bg-[#bdbdbd]">
             30
           </span>
         </p>
       </div>
 
       <ul className="flex w-full gap-4 justify-between p-4 overflow-x-auto">
-        <li className="border-[#00000042] border-solid border-2 px-4 rounded-full">
+        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
           All
         </li>
-        <li className="bg-[var(--bg-active)]  px-4 rounded-full text-white">
+
+        <li className="border-solid border-3 border-[var(--bg-active)] text-sm px-4 py-2 rounded-full bg-[var(--bg-active)] text-white">
           Newest
         </li>
 
-        <li className="border-[#00000042] border-solid border-2 px-4 rounded-full">
+        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
           Popular
         </li>
 
-        <li className="border-[#00000042] border-solid border-2 px-4 rounded-full">
+        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
           Trending
         </li>
 
-        <li className="border-[#00000042] border-solid border-2 px-4 rounded-full">
+        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
           Women
         </li>
 
-        <li className="border-[#00000042] border-solid border-2 px-4 rounded-full">
+        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
           Men
         </li>
       </ul>
