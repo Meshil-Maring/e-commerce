a421ae10330ab39c7c66dbfb8c9b5176617bc8de
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-10 12:18:02 +0530

    Update Sign up

--- src/components/mobile/login/Login.jsx
+++ src/components/mobile/login/Login.jsx
@@ -9,13 +9,13 @@ import Apple from "../../../assets/images/logos/apple.png";
 
 const SignUp = () => {
   return (
-    <section className="flex flex-col justify-between h-[90%] bg-[var(--primary)]">
+    <section className="flex flex-col justify-between h-screen bg-[var(--primary)]">
       <aside className="p-4">
         <Back fill="black" size={28} />
       </aside>
 
-      <div className="p-4 flex flex-col gap-2">
-        <h2 className="text-xl font-bold text-[var(--text)]">Sign up</h2>
+      <div className="p-4 flex flex-col gap-4">
+        <h2 className="text-2xl font-bold text-[var(--text)]">Sign up</h2>
         <p className="text-sm font-semibold">
           Sign up creates an account with your email and password. It lets you
           access personalized features securely.
@@ -23,8 +23,8 @@ const SignUp = () => {
       </div>
 
       <div className="bg-[var(--white)] rounded-t-[32px] mt-4">
-        <form className="p-4 flex flex-col gap-3 mt-4">
-          <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
+        <form className="p-4 flex flex-col gap-4 mt-4">
+          <aside className="bg-[var(--white-100)] h-12 rounded-xl flex items-center px-2 gap-4">
             <Person stroke="var(--white-500)" />
             <input
               type="text"
@@ -33,7 +33,7 @@ const SignUp = () => {
             />
           </aside>
 
-          <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
+          <aside className="bg-[var(--white-100)] h-12 rounded-xl flex items-center px-2 gap-4">
             <MailIcon stroke="var(--white-500)" />
             <input
               type="email"
@@ -42,7 +42,7 @@ const SignUp = () => {
             />
           </aside>
 
-          <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
+          <aside className="bg-[var(--white-100)] h-12 rounded-xl flex items-center px-2 gap-4">
             <KeyIcon stroke="var(--white-500)" size={30} />
             <input
               type="password"
@@ -51,56 +51,46 @@ const SignUp = () => {
             />
             <EyeOpen stroke="var(--white-500)" />
           </aside>
-
-          <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
-            <KeyIcon stroke="var(--white-500)" size={30} />
-            <input
-              type="password"
-              placeholder="Comfirm Password"
-              className="text-sm w-full h-full focus:outline-none text-[var(text)]"
-            />
-            <EyeOpen stroke="var(--white-500)" />
-          </aside>
+          <button className="text-[12px] text-right">Forget Password?</button>
 
           <button
             type="submit"
             className="bg-[var(--text)] text-white p-3 rounded-xl text-sm mt-4"
           >
-            Sign Up
+            Log in
           </button>
         </form>
 
         <div className="mt-4">
           <div className="flex items-center p-2">
             <div className="h-[2px] w-full bg-[var(--white-200)]"></div>
-            <p className="w-full text-sm text-center text-[var(--white-400)]">
-              or Sign-up with
+            <p className="w-full text-center text-[var(--white-400)]">
+              or log in with
             </p>
             <div className="h-[2px] w-full bg-[var(--white-200)]"></div>
           </div>
 
           <div className="flex flex-col gap-2 p-4 justify-center items-center">
-            <button className="flex items-center justify-center gap-2 border-[2px] border-[var(--white-200)] rounded-xl h-11 w-full p-2">
+            <button className="flex items-center justify-center gap-2 border-[2px] border-[var(--white-200)] rounded-xl h-12 w-full p-2">
               <img src={Google} alt="Google" className="w-6 h-6" />
-              <p className="w-full text-sm">Sign up with Google</p>
+              <p className="w-full text-sm">Log in with Google</p>
             </button>
 
-            <button className="flex items-center justify-center gap-2 border-[2px] border-[var(--white-200)] rounded-xl h-11 w-full p-2">
+            <button className="flex items-center justify-center gap-2 border-[2px] border-[var(--white-200)] rounded-xl h-12 w-full p-2">
               <img src={Facebook} alt="Google" className="w-6 h-6" />
-              <p className="w-full text-sm text-[var(--text)]">
-                Sign up with Facebook
-              </p>
+              <p className="w-full text-sm">Log in with Google</p>
             </button>
 
-            <button className="flex items-center justify-center gap-2 border-[2px] border-[var(--white-200)] rounded-xl h-11 w-full p-2">
+            <button className="flex items-center justify-center gap-2 border-[2px] border-[var(--white-200)] rounded-xl h-12 w-full p-2">
               <img src={Apple} alt="Google" className="w-6 h-6" />
-              <p className="w-full text-sm">Sign up with Apple</p>
+              <p className="w-full text-sm">Log in with Google</p>
             </button>
 
             <p className="text-[var(--white-400)] mt-4 text-sm">
-              Do have an accout?
+              Don't have an accout?
               <button className="ml-2 text-[var(--text)] font-bold">
-                Log in
+                {" "}
+                Sign Up
               </button>
             </p>
           </div>
