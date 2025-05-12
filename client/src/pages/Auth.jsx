d0f6ce4d8665b40295da3534be56754085eac398
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-12 18:18:02 +0530

    update something

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,12 +1,12 @@
 import { use, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, data } from "react-router-dom";
 
 import Back from "../assets/icons/Back";
 import MailIcon from "../assets/icons/mail";
 import KeyIcon from "../assets/icons/key";
 import Person from "../assets/icons/person";
-import EyeOpen from "../assets/icons/EyeOpen";
 import EyeCloseIcon from "../assets/icons/EyeClose";
+import EyeOpenIcon from "../assets/icons/EyeOpen";
 import Google from "../assets/images/logos/google.png";
 import Facebook from "../assets/images/logos/facebook.png";
 import Apple from "../assets/images/logos/apple.png";
@@ -18,6 +18,55 @@ const authType = {
 
 const AuthPage = () => {
   const [hidePassword, setHidePassword] = useState(true);
+
+  // TODO: Auth Erro useState();
+  const [authErr, setAuthErr] = useState({
+    userName: {
+      err: false,
+      message: "Please enter your name",
+    },
+
+    email: {
+      err: false,
+      message: "Please enter a valid email address.",
+    },
+
+    password: {
+      err: false,
+      message: "Please create a new password as per the password criteria.",
+    },
+  });
+
+  const [authData, setAuthData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const changeAuthData = (e) => {
+    const { name, value } = e.target;
+
+    setAuthData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // handler the submitted form
+  // TODO:
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const isUserNameEmpty = authData.userName.trim().length === 0;
+
+    setAuthErr((prev) => ({
+      ...prev,
+      userName: {
+        ...prev.userName,
+        err: isUserNameEmpty,
+      },
+    }));
+  };
 
   const type = useParams().type;
 
@@ -53,14 +102,23 @@ const AuthPage = () => {
         <div className="bg-[var(--white)] rounded-t-[32px] mt-4">
           <form className="p-4 flex flex-col gap-3 mt-4">
             {type == authType.signup && (
-              <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
-                <Person stroke="var(--white-500)" />
-                <input
-                  type="text"
-                  placeholder="User name"
-                  className="text-sm w-full h-full focus:outline-none text-[var(text)]"
-                />
-              </aside>
+              <div className="flex flex-col gap-1">
+                <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
+                  <Person stroke="var(--white-500)" />
+
+                  <input
+                    onChange={changeAuthData}
+                    name="userName"
+                    type="text"
+                    placeholder="User name"
+                    className="text-sm w-full h-full focus:outline-none text-[var(text)]"
+                  />
+                </aside>
+
+                <p className="text-[10px] text-red-600">
+                  {authErr.userName.err && authErr.userName.message}
+                </p>
+              </div>
             )}
 
             <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
@@ -79,7 +137,7 @@ const AuthPage = () => {
                 placeholder="Password"
                 className="text-sm w-full h-full focus:outline-none text-[var(text)]"
               />
-              {/* TODO: */}
+
               <button
                 onClick={(e) => {
                   e.preventDefault();
@@ -87,30 +145,22 @@ const AuthPage = () => {
                 }}
               >
                 {hidePassword ? (
-                  <p className="text-sm p-1">Show</p>
+                  <EyeCloseIcon fill={"var(--white-400)"} size={18} />
                 ) : (
-                  <p className="text-sm">Hide</p>
+                  <EyeOpenIcon stroke="var(--white-400)" size={18} />
                 )}
               </button>
             </aside>
 
-            {type == authType.signup && (
-              <aside className="bg-[var(--white-100)] h-11 rounded-xl flex items-center px-2 gap-4">
-                <KeyIcon stroke="var(--white-500)" size={30} />
-                <input
-                  type={hidePassword ? "password" : "text"}
-                  placeholder="Comfirm Password"
-                  className="text-sm w-full h-full focus:outline-none text-[var(text)]"
-                />
-              </aside>
-            )}
             {type == authType.login && (
               <button className="text-right text-[var(--text)] text-sm font-semibold">
                 Forget Password?
               </button>
             )}
 
+            {/* TODO: Submit the form */}
             <button
+              onClick={submitHandler}
               type="submit"
               className="bg-[var(--text)] text-white p-3 rounded-xl text-sm mt-4"
             >
