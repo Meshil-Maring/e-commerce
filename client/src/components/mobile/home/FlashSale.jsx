d68f540ca337ad3ba474752b62317c02eca4b572
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-20 15:12:01 +0530

    Update flash sale

--- client/src/components/mobile/home/FlashSale.jsx
+++ client/src/components/mobile/home/FlashSale.jsx
@@ -2,8 +2,8 @@ const FlashSale = () => {
   return (
     <div>
       <div className="flex w-full justify-between items-center p-4 py-0 mt-8">
-        <h3 className="text-md font-medium">Flash Sale</h3>
-        <p className="flex gap-2 text-sm justify-center items-center text-[var(--text-secondary)]">
+        <h3 className="text-md text-lg">Flash Sale</h3>
+        <p className="flex gap-2 text-lg justify-center items-center text-[var(--text-secondary)]">
           Closing in:
           <span className="p-2 text-sm px-2 rounded-md text-white bg-[#bdbdbd]">
             02
@@ -16,27 +16,27 @@ const FlashSale = () => {
       </div>
 
       <ul className="flex w-full gap-4 justify-between p-4 overflow-x-auto">
-        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
+        <li className="border-[#00000042] border-solid border-2 text-sm px-4 py-2 rounded-full">
           All
         </li>
 
-        <li className="border-solid border-3 border-[var(--bg-active)] text-sm px-4 py-2 rounded-full bg-[var(--bg-active)] text-white">
+        <li className="border-solid border-2 border-[var(--text)] text-sm px-4 py-2 rounded-full bg-[var(--text)] text-[var(--white)]">
           Newest
         </li>
 
-        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
+        <li className="border-[#00000042] border-solid border-2 text-sm px-4 py-2 rounded-full">
           Popular
         </li>
 
-        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
+        <li className="border-[#00000042] border-solid border-2 text-sm px-4 py-2 rounded-full">
           Trending
         </li>
 
-        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
+        <li className="border-[#00000042] border-solid border-2 text-sm px-4 py-2 rounded-full">
           Women
         </li>
 
-        <li className="border-[#00000042] border-solid border-3 text-sm px-4 py-2 rounded-full">
+        <li className="border-[#00000042] border-solid border-2 text-sm px-4 py-2 rounded-full">
           Men
         </li>
       </ul>
--- client/src/components/mobile/home/location-component.jsx
+++ client/src/components/mobile/home/location-component.jsx
@@ -5,7 +5,7 @@ import ChevronDownIcon from "../../../assets/icons/ChevronDownIcon";
 const LocationComponent = () => {
   return (
     <div className="flex justify-center">
-      <button className="bg-[var(--bg-surface)] flex gap-2 justify-between items-center w-[90%] p-2 border-t-[3px] border-black/20 rounded-b-[12px]">
+      <button className="bg-[var(--bg-surface)] flex gap-2 justify-between items-center w-[90%] p-2 border-t-[2px] border-black/20 rounded-b-[12px]">
         <LocationIcon size={24} fill="none" stroke="#1f2937" strokeWidth={1} />
         <p className="w-full text-[var(--text-color)] text-sm font-meduim text-md">
           Deliver to
