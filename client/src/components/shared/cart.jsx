ced67ba0189aac2e9a687f22b9a4b203ad784778
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-23 10:56:35 +0530

    update header

--- client/src/components/shared/cart.jsx
+++ client/src/components/shared/cart.jsx
@@ -4,7 +4,7 @@ import CartIcon from "../../assets/icons/CartIcon";
 const Cart = () => {
   return (
     <button className="text-zinc-900 relative">
-      <p className=" absolute right-[-4px] top-[-4px] rounded-full bg-green-700 w-5 h-5 text-[10px] flex justify-center items-center text-white">
+      <p className=" absolute right-[-4px] rounded-full bg-green-700 w-4 h-4 text-[10px] flex justify-center items-center text-white">
         3
       </p>
       <CartIcon fill="#1F2937" size={28} />
--- client/src/components/shared/header.jsx
+++ client/src/components/shared/header.jsx
@@ -1,28 +1,37 @@
-
 import Cart from "../shared/cart";
 import MenuIcon from "../../assets/icons/MenuIcon";
 import BackIcon from "../../assets/icons/back";
 import SearchIcon from "../../assets/icons/SearchIcon";
+import PersonIcon from "../../assets/icons/person";
 
-const Header = ({menu = false}) => {
+const Header = ({ menu = false }) => {
   return (
     <header
       className="text-red-700 flex
     justify-between p-3 w-full bg-[var(--bg-surface)]"
     >
-    {menu ? <button className="text-zinc-900 text-3xl">
-    <MenuIcon size={28} />
-    </button> : <button>
-        <BackIcon/>
-        </button>}
-
+      {menu ? (
+        <button className="text-zinc-900 text-3xl">
+          <MenuIcon size={28} />
+        </button>
+      ) : (
+        <button>
+          <BackIcon />
+        </button>
+      )}
 
       <div className="flex gap-4">
         <button className="text-zinc-900 text-3xl">
           <SearchIcon size={28} />
         </button>
 
-        <Cart size={28} />
+        <button>
+          <Cart size={28} />
+        </button>
+
+        <button className="flex rounded-full w-7 h-7 items-center justify-center bg-[var(--text)]">
+          <PersonIcon stroke="white" size={22} />
+        </button>
       </div>
     </header>
   );
