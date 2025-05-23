c8609299cf9609d2725dd52b4d5e1b798f505931
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-23 15:29:45 +0530

    Add product-details image preview

--- client/src/pages/Product-details.jsx
+++ client/src/pages/Product-details.jsx
@@ -1,20 +1,11 @@
 import Header from "../components/shared/header";
-import SurfCuz from "../assets/images/surf-cuz.png";
-import ShareIcon from "../assets/icons/share";
-import HeartIcon from "../assets/icons/HeartIcon";
+import ImagePreview from "../components/mobile/product-details/Image-preview";
 
 const ProductDetails = () => {
   return (
     <>
       <Header />
-
-      <div className="bg-[#C8C8C8] m-4 rounded-2xl">
-        <img className="bg-cover" src={SurfCuz} alt="surf cuz"></img>
-      </div>
-      <aside>
-        <ShareIcon size={24} strokeW="1.5" />
-        <HeartIcon size={24} strokeW="1.5" />
-      </aside>
+      <ImagePreview />
     </>
   );
 };
