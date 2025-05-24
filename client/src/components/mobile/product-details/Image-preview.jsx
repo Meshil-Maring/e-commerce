4e94644639eca1ef015f4c97b0e6b8b4de6e30c2
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-24 22:06:39 +0530

    Update something

--- client/src/components/mobile/product-details/Image-preview.jsx
+++ client/src/components/mobile/product-details/Image-preview.jsx
@@ -1,15 +1,17 @@
 import SurfCuz from "../../../assets/images/surf-cuz.png";
 import ShareIcon from "../../../assets/icons/share";
 import HeartIcon from "../../../assets/icons/HeartIcon";
+import CommentIcon from "../../../assets/icons/comments";
 
 const ImagePreview = () => {
   return (
     <div className="bg-[#C8C8C8] m-4 rounded-2xl relative">
       <img className="bg-cover" src={SurfCuz} alt="surf cuz"></img>
 
-      <aside className="flex gap-4 flex-col absolute top-6 right-4">
+      <aside className="flex gap-3 flex-col absolute top-6 right-4">
         <HeartIcon size={24} strokeW="1.5" stroke="var(--secondary)" />
         <ShareIcon size={24} strokeW="1.5" stroke="var(--secondary)" />
+        <CommentIcon size={24} strokeW="1.5" stroke="var(--secondary)" />
       </aside>
     </div>
   );
