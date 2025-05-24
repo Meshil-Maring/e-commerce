6fb9eb3fe6c706a22e9d074e211f19e88880d894
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-24 21:54:43 +0530

    Update product variant

---
+++ client/src/assets/icons/Info.jsx
@@ -0,0 +1,24 @@
+const Icon = ({
+  size = 24,
+  fill = "none",
+  stroke = "currentColor",
+  strokeW = "1.5",
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill={fill}
+    stroke={stroke}
+    strokeWidth={strokeW}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="12" y1="16" x2="12" y2="12"></line>
+    <line x1="12" y1="8" x2="12.01" y2="8"></line>
+  </svg>
+);
+
+export default Icon;
--- client/src/components/mobile/product-details/Product-Variants.jsx
+++ client/src/components/mobile/product-details/Product-Variants.jsx
@@ -1,16 +1,19 @@
+import InfoIcon from "../../../assets/icons/Info";
 import SurPink from "../../../assets/images/sur-cuz-pink.png";
 import SurWhite from "../../../assets/images/surf-cuz.png";
 
 const ProductVariants = () => {
   return (
     <ul className="mt-8 ml-4 flex gap-4">
-      <li className="flex flex-col items-center">
+      <li className="flex flex-col items-center relative">
         <img
           className="w-16 h-16 border-2 rounded-2xl border-[var(--white-300)]"
           src={SurPink}
           alt="Surf cuz pink color variants"
         />
-        <p className="text-sm font-bold">₹879</p>
+        <p className="text-sm font-bold">
+          <sm className="text-red-600">-5%</sm> ₹879
+        </p>
         <del className="text-sm text-[var(--white-400)]">₹999</del>
       </li>
 
@@ -20,7 +23,9 @@ const ProductVariants = () => {
           src={SurWhite}
           alt="Surf cuz pink color variants"
         />
-        <p className="text-sm font-bold">₹879</p>
+        <p className="text-sm font-bold">
+          <sm className="text-red-600">-5%</sm> ₹879
+        </p>
         <del className="text-sm text-[var(--white-400)]">₹999</del>
       </li>
     </ul>
