6bf30d7c8818c9c86e23d9187e125c39f31f90fe
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-05-26 22:12:34 +0530

    Update something

--- client/src/components/mobile/product-details/Rating.jsx
+++ client/src/components/mobile/product-details/Rating.jsx
@@ -1,11 +1,13 @@
-import StarIcon from "../../../assets/icons/Star";
+import StarIcon from "../../../assets/icons/StarFill";
 
 const Rating = () => {
   return (
-    <div className="flex gap-1 justify-end items-end">
-      <StarIcon />
-      <p className="text-lg font-bold">4.4</p>
-      <p className="text-sm text-[var(--white-400)]">(41, 238)</p>
+    <div className="flex gap-1 justify-end  items-center ">
+      <aside className="bg-[var(--red)] flex gap-1 px-2 rounded-sm items-center">
+        <StarIcon fill="white" size={20} />
+        <p className="text-lg font-bold text-white">4.4</p>
+      </aside>
+      <p className="text-sm text-[var(--white-500)]">(41, 238)</p>
     </div>
   );
 };
