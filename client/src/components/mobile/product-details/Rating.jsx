f33bda9f7b2051d0446ef3f9f0b732a5b9242be7
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-06-09 21:38:41 +0530

    Update something

--- client/src/components/mobile/product-details/Rating.jsx
+++ client/src/components/mobile/product-details/Rating.jsx
@@ -3,9 +3,9 @@ import StarIcon from "../../../assets/icons/StarFill";
 const Rating = () => {
   return (
     <div className="flex gap-1 justify-end  items-center ">
-      <aside className="bg-[var(--red)] flex gap-1 px-2 rounded-sm items-center">
-        <StarIcon fill="white" size={20} />
-        <p className="text-lg font-bold text-white">4.4</p>
+      <aside className="bg-[var(--red)] flex px-1 rounded-sm justify-center items-center">
+        <StarIcon fill="white" size={18} />
+        <p className="text-sm font-bold text-white pr-1">4.4</p>
       </aside>
       <p className="text-sm text-[var(--white-500)]">(41, 238)</p>
     </div>
